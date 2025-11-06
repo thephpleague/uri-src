7a3fdb645b6a32c094f841257e2c1de25ddbff38
ignace nyamagana butera <nyamsprod@gmail.com>
2025-11-06 08:58:55 +0100

    Adding On this page generated navigation

--- docs/scripts.0003.js
+++ docs/scripts.0003.js
@@ -1,6 +1,67 @@
 (() => {
   let contentHeaders= document.querySelectorAll("main h2[id]");
-  if (!document.querySelector('html').classList.contains('homepage') && contentHeaders) {
+  if (!document.querySelector('html').classList.contains('homepage')  && contentHeaders) {
+    const headings = [...document.querySelector('article.content').querySelectorAll('h2, h3, h4, h5, h6')];
+    console.log(headings.length);
+    if (headings.length > 0) {
+      const aside = document.createElement('aside');
+      const tocParent = document.createElement('nav');
+      aside.setAttribute("class", "sticky top-[4.5rem] h-[calc(100vh-4.5rem)] w-72 overflow-y-auto pr-8 text-sm xl:pr-16 self-start hidden lg:block");
+      const tocParentTitle = document.createElement('h3');
+      tocParentTitle.textContent = 'On this page';
+      tocParentTitle.classList.add("font-semibold", "tracking-tight", "text-slate-900");
+      const toc = document.createElement('ul');
+      toc.classList.add("pl-3", "mt-3", "space-y-2");
+      let currentLevel = 1;
+      let currentList = toc;
+
+      headings.forEach(h => {
+        const level = parseInt(h.tagName.slice(1), 10);
+
+        // Going deeper (h2 -> h4)
+        while (level > currentLevel) {
+          const newList = document.createElement('ul');
+          newList.classList.add("pl-3", "mt-3", "space-y-2");
+
+          // If there is no lastElementChild, create a dummy parent <li>
+          if (!currentList.lastElementChild) {
+            const placeholder = document.createElement('li');
+            currentList.appendChild(placeholder);
+          }
+
+          currentList.lastElementChild.appendChild(newList);
+          currentList = newList;
+          currentLevel++;
+        }
+
+        // Going shallower (h4 -> h2)
+        while (level < currentLevel) {
+          currentList = currentList.parentElement.closest('ul');
+          currentLevel--;
+        }
+
+        // Add list item for current heading
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+
+        if (!h.id) {
+          h.id = h.textContent.trim().toLowerCase().replace(/\s+/g, '-');
+        }
+
+        a.href = `#${h.id}`;
+        a.textContent = h.textContent;
+        a.classList.add("block", "hover:underline");
+        li.appendChild(a);
+        currentList.appendChild(li);
+
+      });
+
+
+      tocParent.append(tocParentTitle, toc);
+      aside.append(tocParent);
+      document.querySelector('main').append(aside);
+    }
+
     const uri = new URL(location.href);
     contentHeaders.forEach((header) => {
       uri.hash = header.id;
